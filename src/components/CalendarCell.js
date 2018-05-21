f869c7b7a61c016e871d0df89e3a6452b78f813b
Alexis Duran <alexis.duranm@gmail.com>
2018-05-21 11:15:13 -0400

    Add CalendarCell basic files

---
+++ src/components/CalendarCell.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./CalendarCell.css";
+
+class CalendarCell extends Component {
+  render() {
+    return <div className="calendar-cell">calendar cell</div>;
+  }
+}
+
+export default CalendarCell;
